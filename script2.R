9fe5369df346238d4234f2d9b7c1085e8aa19a22
Jeremy Simon <Jeremy@Jeremys-Laptop.local>
2019-07-08 14:24:59 +0200

    updated files

--- script2.R
+++ script2.R
@@ -1,1 +1,22 @@
 y <- 1000
+
+
+
+#to put up files on git
+#go to terminal
+#git add fn
+#"git add script.R script2.R"
+
+#this puts files in staging area
+#"git status" while show you staging area
+#added files will not reflect changes -- re-add to get changes!
+
+#to commit files to git: "git commit"
+#puts you into VIM editor...this should be avoided
+#this editor is where you write in what your changes are
+#can put a one line description with:
+# git commit -m "your comment"
+
+#then can type in "git log"
+#shows info
+#hm, not actually showing my email. strange
